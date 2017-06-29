7576708e019246d4001d417e9d0513fc910945cc
Steven Than <steventhan11@gmail.com>
2017-06-29 09:16:02 -0400

    displayed NavigationBar and its menu items

--- hvtilegrout/src/App.js
+++ hvtilegrout/src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { getMuiTheme, MuiThemeProvider } from "material-ui/styles";
+import { BrowserRouter, Route } from "react-router-dom";
+import injectTapEventPlugin from "react-tap-event-plugin";
+
+import theme from "./theme";
+import NavigationBar from "./components/menus/NavigationBar";
 import "./App.css";
+
+injectTapEventPlugin();
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <MuiThemeProvider muiTheme={getMuiTheme(theme)}>
+        <div className="App">
+          <NavigationBar />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
