99b500846c8a62639fb5284c3bc5b592ef785b48
Steven Than <steventhan11@gmail.com>
2017-06-29 11:15:18 -0400

    add ReactRouter to main App

--- hvtilegrout/src/App.js
+++ hvtilegrout/src/App.js
@@ -5,6 +5,10 @@ import injectTapEventPlugin from "react-tap-event-plugin";
 
 import theme from "./theme";
 import NavigationBar from "./components/menus/NavigationBar";
+import HomePage from "./components/home/HomePage";
+import AboutPage from "./components/about/AboutPage";
+import GalleryPage from "./components/gallery/GalleryPage";
+import ContactPage from "./components/contact/ContactPage";
 import "./App.css";
 
 injectTapEventPlugin();
@@ -13,9 +17,15 @@ class App extends Component {
   render() {
     return (
       <MuiThemeProvider muiTheme={getMuiTheme(theme)}>
-        <div className="App">
-          <NavigationBar />
-        </div>
+        <BrowserRouter>
+          <div>
+            <NavigationBar/>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/about" component={AboutPage} />
+            <Route exact path="/gallery" component={GalleryPage} />
+            <Route exact path="/contact" component={ContactPage} />
+          </div>
+        </BrowserRouter>
       </MuiThemeProvider>
     );
   }
