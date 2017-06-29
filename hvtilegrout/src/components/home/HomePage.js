631885445dba33eef40b9ef780083285eb5983d2
Steven Than <steventhan11@gmail.com>
2017-06-29 15:16:46 -0400

    added Header component to HomePage

--- hvtilegrout/src/components/home/HomePage.js
+++ hvtilegrout/src/components/home/HomePage.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import Header from "./Header";
 
 class HomePage extends Component {
   render() {
     return (
-      <div>
-        <h1>This is homepage</h1>
-      </div>
+      <main className="home">
+        <Header/>
+      </main>
     );
   }
 }
