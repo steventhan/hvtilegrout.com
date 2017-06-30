233cdd122b6318750b087b353c03ea8a84f133bb
Steven Than <steventhan11@gmail.com>
2017-06-30 05:29:52 -0400

    moved inline styling of HomePage Header to a separate css file

--- hvtilegrout/src/components/home/Header.js
+++ hvtilegrout/src/components/home/Header.js
@@ -1,25 +1,12 @@
 import React, { Component } from "react";
 import { RaisedButton } from "material-ui";
+import "./Header.css";
 
 class Header extends Component {
   render() {
-    let headerStyle = {
-      height: "100%",
-      display: "flex",
-      flexDirection: "row",
-      alignItems: "flex-start",
-      justifyContent: "center",
-    };
-
-    let headerItem = {
-      selfAlign: "auto",
-      maxWidth: "70%",
-      textAlign: "center",
-    };
-
     return (
-      <section style={headerStyle} className="header">
-        <div style={headerItem}>
+      <section className="header">
+        <div className="container">
           <h1>HV Tile Grout</h1>
           <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ex quam, suscipit ut ipsum ultrices,
           mattis tempor nisl. Nulla tempor, nisi vitae sagittis vulputate, mi nisi bibendum magna, id bibendum odio elit
