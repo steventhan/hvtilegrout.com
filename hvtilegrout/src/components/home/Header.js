a1ef3e7ae15c40fc829a5ba93c564bf81d257a42
Steven Than <steventhan11@gmail.com>
2017-06-30 18:55:16 -0400

    hide nav-items on mobile, need to work on a hamburger icon

--- hvtilegrout/src/components/home/Header.js
+++ hvtilegrout/src/components/home/Header.js
@@ -1,18 +1,23 @@
 import React, { Component } from "react";
 import { RaisedButton } from "material-ui";
 import "./Header.css";
+import background from "../../header-bg.jpg";
 
 class Header extends Component {
   render() {
     return (
-      <section className="header">
+      <section style={{backgroundImage: `url(${background})`}} className="header">
         <div className="container">
           <h1>HV Tile Grout</h1>
           <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ex quam, suscipit ut ipsum ultrices,
           mattis tempor nisl. Nulla tempor, nisi vitae sagittis vulputate, mi nisi bibendum magna, id bibendum odio elit
           ac ante. Donec euismod, ante ut varius vulputate, nisi velit sollicitudin ligula, commodo auctor justo felis
           sit amet dolor.</h3>
-          <RaisedButton label="Featured Works" secondary={true} />
+          <RaisedButton
+            style={{height: 50, width: 250}}
+            label="Featured Works"
+            labelStyle={{fontSize: 18}}
+            secondary={true} />
         </div>
       </section>
     );
