f58e13c73d591c841ab9d5a322c6e5c5b499be63
Steven Than <steventhan11@gmail.com>
2017-06-30 07:02:16 -0400

    added OurTeam component for HomePage

---
+++ hvtilegrout/src/components/home/OurTeam.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./OurTeam.css";
+
+class OurTeam extends Component {
+  render() {
+    return (
+      <section className="our-team">
+        <div className="container">
+          <h1>Our Team</h1>
+          <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ex quam, suscipit ut ipsum ultrices,
+          mattis tempor nisl. </h3>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default OurTeam;
