004bb2d56175f914979c3228f8878abfb66d0251
Steven Than <steventhan11@gmail.com>
2017-07-26 06:06:29 -0400

    added SectionHeaderText component for styling section header

---
+++ hvtilegrout/src/components/menus/SectionHeaderText.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+class SectionHeaderText extends Component {
+  constructor(props) {
+    super(props);
+    this.style = {
+      display: "flex",
+      justifyContent: this.props.center ? "center" : "flex-start"
+    };
+    this.borderStyle = {
+      marginLeft: this.props.center ? "15%" : "0"
+    };
+  }
+
+  render() {
+    return (
+      <div style={this.style} className="header-text">
+        <h1>
+          {this.props.text}
+          <div style={this.borderStyle} className="border"></div>
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default SectionHeaderText;
