cdde3baa0945436da4612a99da819968cbdc9535
Steven Than <steventhan11@gmail.com>
2017-07-30 05:38:35 -0400

    added real projects to FeaturedWorks, styling adjustments

--- hvtilegrout/src/components/home/FeaturedWork.js
+++ hvtilegrout/src/components/home/FeaturedWork.js
@@ -10,12 +10,14 @@ class FeaturedWork extends Component {
   constructor(props) {
     super(props);
     this.images = [
-      {src: "http://i.imgur.com/kwNKVWc.jpg"},
-      {src: "http://i.imgur.com/wDi6zYv.jpg"},
-      {src: "http://i.imgur.com/F9uBiUY.jpg"},
-      {src: "http://i.imgur.com/F9uBiUY.jpg"},
-      {src: "http://i.imgur.com/F9uBiUY.jpg"},
-      {src: "http://i.imgur.com/MFjDnru.jpg"}
+      {src: "http://i.imgur.com/TdpiiC5.jpg"},
+      {src: "http://i.imgur.com/vcChO7L.jpg"},
+      {src: "http://i.imgur.com/8BXqdTY.jpg"},
+      {src: "http://i.imgur.com/2VupPkK.png"},
+      {src: "http://i.imgur.com/WkpnLy7.png"},
+      {src: "http://i.imgur.com/xgeG4ew.png"},
+      {src: "http://i.imgur.com/o9jBibu.png"},
+      {src: "http://i.imgur.com/NDhlDIZ.png"},
     ];
   }
 
