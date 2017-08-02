edecaa6cdabd2c0e0647c4cf324d3d29759b4bad
Steven Than <steventhan11@gmail.com>
2017-08-02 05:28:31 -0400

    add a FloatingActionButton that allows scrolling back up to top on all pages

--- hvtilegrout/src/components/menus/NavigationMenu.js
+++ hvtilegrout/src/components/menus/NavigationMenu.js
@@ -1,17 +1,20 @@
 import React, { Component } from "react";
+import { FloatingActionButton } from "material-ui";
+import ArrowUp from "material-ui/svg-icons/navigation/arrow-upward";
+
 import NavigationBar from "./NavigationBar";
 import NavigationDrawer from "./NavigationDrawer";
 
+const menuLinks = [
+  {text: "Home", url: "/"},
+  {text: "About", url: "/about"},
+  {text: "Gallery", url: "/gallery"},
+  {text: "Contact", url: "/contact"},
+];
 
 class NavigationMenu extends Component {
   constructor(props) {
     super(props);
-    this.menuLinks = [
-      {text: "Home", url: "/"},
-      {text: "About", url: "/about"},
-      {text: "Gallery", url: "/gallery"},
-      {text: "Contact", url: "/contact"},
-    ];
     this.state = {drawerOpen: false};
     this.handleHamburgerTouchTap = this.handleHamburgerTouchTap.bind(this);
     this.closeDrawer = this.closeDrawer.bind(this);
@@ -28,11 +31,17 @@ class NavigationMenu extends Component {
   render() {
     return (
       <div>
-        <NavigationBar onHamburgerTouchTap={this.handleHamburgerTouchTap} menuLinks={this.menuLinks} />
-        <NavigationDrawer drawerOpen={this.state.drawerOpen} closeDrawer={this.closeDrawer} menuLinks={this.menuLinks}/>
+        <NavigationBar onHamburgerTouchTap={this.handleHamburgerTouchTap} menuLinks={menuLinks} />
+        <NavigationDrawer drawerOpen={this.state.drawerOpen} closeDrawer={this.closeDrawer} menuLinks={menuLinks}/>
+        <FloatingActionButton
+          style={{position: "fixed", bottom: 20, right: 20, zIndex: 99}}
+          onTouchTap={e => window.scrollTo(0, 0)}>
+          <ArrowUp />
+        </FloatingActionButton>
       </div>
     );
   }
 }
 
 export default NavigationMenu;
+export { menuLinks };
