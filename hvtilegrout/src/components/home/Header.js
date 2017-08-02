d8cdbd78530af3741688e6edb5e53e4fb68e73b5
Steven Than <steventhan11@gmail.com>
2017-08-02 05:27:26 -0400

    changed out h3 tag with p tag for header paragraph

--- hvtilegrout/src/components/home/Header.js
+++ hvtilegrout/src/components/home/Header.js
@@ -9,11 +9,12 @@ class Header extends Component {
       <section style={{backgroundImage: `url(${background})`}} className="header">
         <div className="container">
           <h1>HV Tile Grout</h1>
-          <h3>​HV Grout & Tile is the number one source in the Bay Area region and your first choice for all your
+          <p>​
+            HV Grout & Tile is the number one source in the Bay Area region and your first choice for all your
             Stone & Tile Restoration needs. Tile and Grout cleaning, Grout and Natural Stone Sealing, Shower
             Regrouting, and all other cleaning and restoration service.  We offer a complete line of cleaning and
             restoration solutions to for all types of tile, grout and stone.
-          </h3>
+          </p>
           <RaisedButton
             style={{height: 50, width: 250}}
             label="Featured Works"
